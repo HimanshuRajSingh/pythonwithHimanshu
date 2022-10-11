ac38beb14bcb7f165af6483a349f0f5cfc97e184
Himanshu Raj <Himanshurajsingh1997@gmail.com>
2022-10-11 22:10:14 +0530

    Added basic python script

---
+++ python-basics/comments.py
@@ -0,0 +1,8 @@
+#single line comment : #
+#multi-line comment : ''''''
+
+# fvhgjmhgjgjfghjfh
+
+
+print('''Himanshu is a good boy,
+       Tannu is also good student''')
---
+++ python-basics/inputex.py
@@ -0,0 +1,7 @@
+a=input("Enter the value of a:")
+print(a)
+print(type(a))
+
+num=float(input("Enter a Integer number : "))
+print(num)
+print(type(num))
