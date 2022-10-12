d2f359520648bfc9018c5688029aabaf10cca65b
Himanshu Raj <Himanshurajsingh1997@gmail.com>
2022-10-12 21:11:45 +0530

    Created and modified file

---
+++ python-basics/d2String1.py
@@ -0,0 +1,16 @@
+str="tannu"
+str1='Himanshu'
+str2='''Himanshu
+        Akkku'''
+print(str)
+print(str1)
+print(str2)
+
+print(str[1])
+print(str[-5])
+print(len(str))
+str_slice=str1[2:4]
+print(str_slice)
+
+str_slice=str1[2:7:2]
+print(str_slice)
