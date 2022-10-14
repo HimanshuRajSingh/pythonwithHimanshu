5f228957e7def25fb8391736378cb2459cb35719
Himanshu Raj <Himanshurajsingh1997@gmail.com>
2022-10-15 00:25:06 +0530

    Added tuple related example

---
+++ python-basics/d4dict-methods.py
@@ -0,0 +1,10 @@
+dic={1:1,2:2,3:3,4:4}
+print(dic.items())
+print(dic.keys())
+print(dic.values())
+
+dic.update({1:"himanshu"})
+print(dic)
+
+
+print(dic.get(1))
---
+++ python-basics/d4dict01.py
@@ -0,0 +1,11 @@
+dict1={1:"apple",2:"cat",3:"animal"}
+
+print(dict1[1])
+
+dict2={1:"pallo",2:"akku","list":["tannu","pra"]}
+#dict2["list"]= "abc"
+
+print(dict2[2] , dict2[1])
+print(dict2["list"])
+
+
