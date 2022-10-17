b8938016f5fa979678223530e916f2cf0a42b50d
Himanshu Raj <Himanshurajsingh1997@gmail.com>
2022-10-17 22:47:25 +0530

    Added example of sets

---
+++ python-basics/d5set-method.py
@@ -0,0 +1,10 @@
+set1={1,8,2,3}
+print(len(set1))
+
+set1.remove(1)
+print(set1)
+
+#set1.pop()
+print(set1.pop())
+
+print(set1.pop())
---
+++ python-basics/d5set01.py
@@ -0,0 +1,7 @@
+set1={2,4,5,6}
+print(set1)
+set1.add(1)
+print(set1)
+
+set1.pop()
+print(set1)
