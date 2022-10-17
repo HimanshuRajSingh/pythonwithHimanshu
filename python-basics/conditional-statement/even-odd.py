b5a71ebc48cdcef32917bd3c0b9de59380f7430a
Himanshu Raj <Himanshurajsingh1997@gmail.com>
2022-10-17 23:09:29 +0530

    Added if-condition examples

---
+++ python-basics/conditional-statement/even-odd.py
@@ -0,0 +1,10 @@
+num=int(input("Enter any positive integer: "))
+
+if num%2==0:
+    b=2
+    c=5
+    print(b+c)
+    print("Even")
+
+else:
+    print("Odd")
---
+++ python-basics/conditional-statement/ex1If.py
@@ -0,0 +1,5 @@
+a=2
+if a>=3:
+    print("Ok")
+else:
+    print("Not Ok")
